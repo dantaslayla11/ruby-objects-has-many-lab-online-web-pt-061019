bbae685a7e4f8f9b49fc110958e5e5498db91930
layla dantas <laylatdantas@gmail.com>
2019-06-20 23:53:50 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @posts = []
+  end 
+  
+  def author
+ author = ()
+end 
+
+def posts 
+  @posts
+end 
+end 
+  
